1048cc27bde4d8fcaeadcd3633a7260db1f97374
greg <greg.sypek@gmail.com>
2022-05-01 18:18:04 +0200

    refactor code part 1

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,44 @@ import { createStore } from "vuex";
 
 export default createStore({
 	state: {
-		counter: 13,
+		api_key: "a9e58a19d0374ddd9a5108b9b5cef1e0",
+		url_base: "http://api.openweathermap.org/data/2.5/",
+		newQuery: "",
+
+		weather: {},
+		currentWeather: null,
+		// if false -  hide result from history list (if someone want search new one and old is printed on the screen - prevent double results)
+		showNew: false,
+		cities: [],
 	},
-	getters: {},
-	mutations: {},
-	actions: {},
+	getters: {
+		dateBuilder() {
+			let d = new Date().toLocaleString();
+			return d;
+		},
+	},
+	mutations: {
+		addToCities(state, payload) {
+			state.cities.push(payload);
+		},
+		addNewQuery(state, payload) {
+			state.newQuery = payload;
+		},
+	},
+	actions: {
+		async fetchWeather(context) {
+			// console.log(context);
+			// Example on how to make an API call using  API key is on Readme.md
+			// context.commit("addNewQuery");
+			console.log("here", context.state);
+			let data = await fetch(
+				`${context.state.url_base}weather?q=${context.state.newQuery}&units=metric&APPID=${context.state.api_key}`
+			)
+				.then((res) => res.json())
+				.then((json) => {
+					console.log(typeof json, json);
+					context.commit("addToCities", json);
+				});
+		},
+	},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,81 +2,93 @@
   <div class="home">
     <main>
       <!-- search input -->
-      <div class="search-box">
+      <form @submit.prevent class="search-box">
         <input
           type="text"
           class="search-bar"
-          placeholder="Search..."
+          placeholder="Enter city..."
           v-model="query"
-          @keypress="fetchWeather"
+          @keyup="addNewQuery(query)"
         />
-      </div>
-      <showWeather :currentWeather="currentWeather" />
+        <button @click="fetchWeather">Search</button>
+      </form>
+      <!-- <showWeather :currentWeather="currentWeather" /> -->
       <!-- check if params is send from history( was clicked in History page on any old weather record) -->
-      <showWeather
+      <!-- <showWeather
         v-if="this.$route.params.name && !showNew"
         :currentWeather="this.$route.params"
-      />
+      /> -->
     </main>
-    <h2>{{ $store.state.counter }}</h2>
+    <!-- <p v-for="(city, index) in cities" :key="index">{{ city.name }}></p> -->
+    <p>{{ newQuery }}</p>
+    <p>{{ cities }}</p>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations, mapActions } from "vuex";
+
 import showWeather from "../components/showWeather.vue";
 export default {
   name: "Home",
   components: { showWeather },
-
   data() {
     return {
-      api_key: "a9e58a19d0374ddd9a5108b9b5cef1e0",
-      url_base: "http://api.openweathermap.org/data/2.5/",
       query: "",
-      weather: {},
-      currentWeather: null,
-      // if false -  hide result from history list (if someone want search new one and old is printed on the screen - prevent double results)
-      showNew: false,
     };
   },
-  methods: {
-    fetchWeather(e) {
-      if (e.key == "Enter") {
-        // Example on how to make an API call using  API key is on Readme.md
-        fetch(
-          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
-        )
-          .then((res) => {
-            console.log("res", res);
-            return res.json();
-          })
-          .then(this.setResultsIntoDb)
-          // clean input field
-          .then((this.query = ""))
-          .then((this.showNew = true));
-      }
-    },
-    dateBuilder() {
-      let d = new Date().toLocaleString();
-      return d;
-    },
+  // data() {
+  //   return {
+  //     api_key: "a9e58a19d0374ddd9a5108b9b5cef1e0",
+  //     url_base: "http://api.openweathermap.org/data/2.5/",
+  //     query: "",
+  //     weather: {},
+  //     currentWeather: null,
+  //     // if false -  hide result from history list (if someone want search new one and old is printed on the screen - prevent double results)
+  //     showNew: false,
+  //   };
+  // },
+  computed: {
+    ...mapState(["cities", "newQuery"]),
+    // async fetchWeather(e) {
+    //   if (e.key == "Enter") {
+    //     // Example on how to make an API call using  API key is on Readme.md
+    //     await fetch(
+    //       `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+    //     )
+    //       .then((res) => res.json())
+    //       .then((json) => console.log(typeof json, json))
+    //       .then((data) => {
+    //         this.addToCities(data);
+    //       });
+    //     //     .then(this.setResultsIntoDb)
+    //     //     // clean input field
+    //     //     .then((this.query = ""))
+    //     //     .then((this.showNew = true));
+    //     // }
+    //   }
+    // },
 
-    setResultsIntoDb(results) {
-      // create object who will be send into db.json file (an example object return from openweathermap is in README.md)
-      let newWeather = {
-        name: results.name,
-        date: this.dateBuilder(),
-        degree: results.main.temp,
-      };
-      fetch("http://localhost:3000/cities", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(newWeather),
-      })
-        // currentWeather will be send as props into showWeathe component
-        .then((this.currentWeather = newWeather))
-        .catch((err) => console.error(err));
-    },
+    // setResultsIntoDb(results) {
+    //   // create object who will be send into db.json file (an example object return from openweathermap is in README.md)
+    //   let newWeather = {
+    //     name: results.name,
+    //     date: this.dateBuilder(),
+    //     degree: results.main.temp,
+    //   };
+    //   fetch("http://localhost:3000/cities", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(newWeather),
+    //   })
+    //     // currentWeather will be send as props into showWeathe component
+    //     .then((this.currentWeather = newWeather))
+    //     .catch((err) => console.error(err));
+    // },
+  },
+  methods: {
+    ...mapMutations(["addToCities", "addNewQuery"]),
+    ...mapActions(["fetchWeather"]),
   },
 };
 </script>
